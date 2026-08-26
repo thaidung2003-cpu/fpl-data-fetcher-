8f5f569712f2ba65e0156c7ebb37686f5eac5a7c
thaidung2003-cpu <thaidung2003@gmail.com>
2026-08-26 22:05:34 +0700

    Update FPLapp.py

--- FPLapp.py
+++ FPLapp.py
@@ -333,7 +333,7 @@ def load_historical_data(file_path):
         if 'team' in df.columns:
             if '24-25' in file_path:
                 team_map = {
-                    1: 'ARS', 2: 'AVL', 3: 'BOU', 4: 'BRE', 5: 'BHA', 
+                    1: 'ARS', 2: 'AVL', 5: 'BOU', 4: 'BRE', 3: 'BHA', 
                     6: 'CHE', 7: 'CRY', 8: 'EVE', 9: 'FUL', 10: 'IPS', 
                     11: 'LEI', 12: 'LIV', 13: 'MCI', 14: 'MUN', 15: 'NEW', 
                     16: 'NFO', 17: 'SOU', 18: 'TOT', 19: 'WHU', 20: 'WOL'
